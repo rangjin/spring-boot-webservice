b8f98e40e185842f6fc63a00959951d9bb4b5f6e
rangjin <heathaze2037@gmail.com>
2021-01-19 10:49:50 +0900

    Mustache 사용 게시물 등록, 조회, 수정, 삭제 화면 구성

--- src/main/java/org/rangjin/springbootwebservice/domain/posts/PostsRepository.java
+++ src/main/java/org/rangjin/springbootwebservice/domain/posts/PostsRepository.java
@@ -1,8 +1,15 @@
 package org.rangjin.springbootwebservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Entity 와 Entity Repository 는 함께 위치해야 함
 // 따라서 도메인 패키지에서 함께 관리
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/org/rangjin/springbootwebservice/service/posts/PostsService.java
+++ src/main/java/org/rangjin/springbootwebservice/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.rangjin.springbootwebservice.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.rangjin.springbootwebservice.domain.posts.Posts;
 import org.rangjin.springbootwebservice.domain.posts.PostsRepository;
+import org.rangjin.springbootwebservice.web.dto.PostsListResponseDto;
 import org.rangjin.springbootwebservice.web.dto.PostsResponseDto;
 import org.rangjin.springbootwebservice.web.dto.PostsSaveRequestDto;
 import org.rangjin.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +40,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
 }
